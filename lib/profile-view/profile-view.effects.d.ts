0ac3a5f455773ba7d453ff4aae4c22780cec4e89
soushians noorghorbani <snoorghorbani@gmail.com>
2018-02-25 12:04:40 +0330

    2.0.10

--- lib/profile-view/profile-view.effects.d.ts
+++ lib/profile-view/profile-view.effects.d.ts
@@ -1,12 +1,12 @@
-import "rxjs/add/operator/map";
-import "rxjs/add/operator/mergeMap";
-import "rxjs/add/operator/switchMap";
-import { Router } from "@angular/router";
-import { Observable } from "rxjs/Observable";
-import { Actions } from "@ngrx/effects";
-import { UserService } from "../services";
-import { GetProfileStart, GetProfileSucceed, GetProfileFailed } from "./profile-view.actions";
-import { RefreshUserInfoAction } from "../dashboard";
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/operator/switchMap';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { Actions } from '@ngrx/effects';
+import { UserService } from '../services';
+import { GetProfileStart, GetProfileSucceed, GetProfileFailed } from './profile-view.actions';
+import { RefreshUserInfoAction } from '../dashboard';
 export declare class ProfileViewEffects {
     private actions$;
     private router;
--- src/profile-view/profile-view.effects.ts
+++ src/profile-view/profile-view.effects.ts
@@ -1,18 +1,18 @@
-﻿import "rxjs/add/operator/map";
-import "rxjs/add/operator/mergeMap";
-import "rxjs/add/operator/switchMap";
-import { Injectable } from "@angular/core";
-import { Router } from "@angular/router";
-import { Observable } from "rxjs/Observable";
-import { Action } from "@ngrx/store";
-import { Actions, Effect, toPayload } from "@ngrx/effects";
-import { of } from "rxjs/observable/of";
-import { Store } from "@ngrx/store";
+﻿import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/operator/switchMap';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { Action } from '@ngrx/store';
+import { Actions, Effect, toPayload } from '@ngrx/effects';
+import { of } from 'rxjs/observable/of';
+import { Store } from '@ngrx/store';
 
-import { UserService } from "../services";
-import { EditProfile_ApiModel } from "../models";
-import { ProfileViewActionTypes, GetProfileStart, GetProfileSucceed, GetProfileFailed } from "./profile-view.actions";
-import { RefreshUserInfoAction } from "../dashboard";
+import { UserService } from '../services';
+import { EditProfile_ApiModel } from '../models';
+import { ProfileViewActionTypes, GetProfileStart, GetProfileSucceed, GetProfileFailed } from './profile-view.actions';
+import { RefreshUserInfoAction } from '../dashboard';
 
 @Injectable()
 export class ProfileViewEffects {
@@ -28,13 +28,9 @@ export class ProfileViewEffects {
 	getProfile$ = this.actions$
 		.ofType(ProfileViewActionTypes.GET_PROFILE_START)
 		.map(toPayload)
-		.switchMap((data: EditProfile_ApiModel.Request) => {
-			debugger;
-			return this.userService
-				.getProfileInformation()
-				.map((res) => new GetProfileSucceed(res))
-				.catch(() => Observable.of(new GetProfileFailed()));
-		});
+		.switchMap((data: EditProfile_ApiModel.Request) => this.userService.getProfileInformation())
+		.map((res) => new GetProfileSucceed(res))
+		.catch(() => Observable.of(new GetProfileFailed()));
 
 	@Effect()
 	refreshUserInfo$ = this.actions$
