111a5da3a0da83f3a86da4d5645d0c3c120f27f0
soushians noorghorbani <snoorghorbani@gmail.com>
2018-03-06 10:41:55 +0330

    2.0.142

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,20 @@
-var gulp = require('gulp'),
-	gulpCopy = require('gulp-copy'),
-	inlineNg2Template = require('gulp-inline-ng2-template');
+var gulp = require("gulp"),
+	gulpCopy = require("gulp-copy"),
+	inlineNg2Template = require("gulp-inline-ng2-template");
 
-gulp.task('copy-src', function() {
-	return gulp.src('src/**/*').pipe(gulpCopy('dist/', { prefix: 1 }));
+gulp.task("copy-src", function() {
+	return gulp.src("src/**/*").pipe(gulpCopy("dist/", { prefix: 1 }));
 });
 
-gulp.task('inline-templates', function() {
+gulp.task("fake-publish", function() {
+	return gulp.src("lib/**/*").pipe(gulpCopy("../starter/node_modules/@soushians/user/lib", { prefix: 1 }));
+});
+
+gulp.task("inline-templates", function() {
 	return gulp
-		.src('dist/**/*.ts')
+		.src("dist/**/*.ts")
 		.pipe(inlineNg2Template({ useRelativePaths: true, indent: 0, removeLineBreaks: true }))
-		.pipe(gulp.dest('dist'));
+		.pipe(gulp.dest("dist"));
 });
 
-gulp.task('default',  gulp.series([ 'copy-src', 'inline-templates' ]));
+gulp.task("default", gulp.series([ "copy-src", "inline-templates" ]));
